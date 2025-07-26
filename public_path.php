37a52cf45dd554f6e2b72830bfaec00f4f8108d4
rafaelcacote <rafael.cacote@gmail.com>
2025-07-25 23:37:40 -0400

    Implement code changes to enhance functionality and improve performance

--- public_path.php
+++ public_path.php
@@ -1,7 +1,10 @@
 <?php
-// Redefine o caminho do public_path para usar public_html na Hostinger
-if (!function_exists('public_path')) {
-    function public_path($path = '') {
+if (
+    !function_exists('public_path') &&
+    (getenv('APP_ON_HOSTINGER') === 'true' || env('APP_ON_HOSTINGER') === true)
+) {
+    function public_path($path = '')
+    {
         $publicPath = __DIR__ . '/public_html';
         return $path ? $publicPath . '/' . $path : $publicPath;
     }
