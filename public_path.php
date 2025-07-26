7c4890efc9e78a4e6742bf9e90fe00425f8eebd0
rafaelcacote <rafael.cacote@gmail.com>
2025-07-25 22:25:57 -0400

    feat: Add public_path function to redefine public path for Hostinger

--- public_path.php
+++ public_path.php
@@ -1,1 +1,8 @@
 <?php
+// Redefine o caminho do public_path para usar public_html na Hostinger
+if (!function_exists('public_path')) {
+    function public_path($path = '') {
+        $publicPath = __DIR__ . '/public_html';
+        return $path ? $publicPath . '/' . $path : $publicPath;
+    }
+}
